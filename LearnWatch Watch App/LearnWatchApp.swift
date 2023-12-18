a28d19b8b164ed1b1ec2d2dae4f727aa1a9eb9eb
MITHUN M R <mithunmr286@gmail.com>
2023-12-18 20:25:17 +0530

    Initial Commit

---
+++ LearnWatch Watch App/LearnWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnWatchApp.swift
+//  LearnWatch Watch App
+//
+//  Created by MRM on 18/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
